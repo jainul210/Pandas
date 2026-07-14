e581ad63c2526358b5efd258fc7e20c0fe33493d
JAINUL ABDEEN <jainulabdeen210@gmail.com>
2026-07-14 20:38:32 +0530

    Refactor DataFrame creation and print statement in a1.py

--- a1.py
+++ a1.py
@@ -1,7 +1,16 @@
 import pandas as pd
 import numpy as np
-arr = np.array([[1, 2], [3, 4]])
-df = pd.DataFrame(arr, columns=["A", "B"])
-# print(df)
-a = df["A"]
-print(a)
+# arr = np.array([[1, 2], [3, 4]])
+# df = pd.DataFrame(arr, columns=["A", "B"])
+# # print(df)
+# a = df["A"]
+# print(a)
+
+data = {
+    "Name" : ["Banti", "Sumit", "Robin"],
+    "attr" : [23, 25, 67],
+    "Email" : ["bantihero@gmail.com", "sumit92329@yahoo.com", "robinhood@gmial.com"]
+}
+df = pd.DataFrame(data)
+
+print(df)
