b8ef64c8540948b3d692f35f899c4f9206972ec1
JAINUL ABDEEN <jainulabdeen210@gmail.com>
2026-07-16 13:40:45 +0530

    Refactor DataFrame creation and remove unused student scores DataFrame

--- a1.py
+++ a1.py
@@ -29,14 +29,28 @@ import numpy as np
 # df["Rank"] = df["Age"].rank() 
 # print(df)
 
-data = {
-'Name': ['Alice', 'Bob', 'Charlie'],
-'Math': [85, 78, 92],
-'Science': [90, 82, 89],
-'English': [88, 85, 94]
-}
-df = pd.DataFrame(data)
+# data = {
+# 'Name': ['Alice', 'Bob', 'Charlie'],
+# 'Math': [85, 78, 92],
+# 'Science': [90, 82, 89],
+# 'English': [88, 85, 94]
+# }
+# df = pd.DataFrame(data)
 
 
-df = df.melt(id_vars=["Name"], value_vars=["Math", "Science", "English"], var_name="Subject", value_name="Score")
+# df = df.melt(id_vars=["Name"], value_vars=["Math", "Science", "English"], var_name="Subject", value_name="Score")
+# print(df)
+
+df = pd.DataFrame({
+"Department": ["HR", "HR", "IT", "IT", "Marketing", "Marketing", "Sales", "Sales"],
+"Team": ["A", "A", "B", "B", "C", "C", "D", "D"],
+"Gender": ["M", "F", "M", "F", "M", "F", "M", "F"],
+"Salary": [85, 90, 78, 85, 92, 88, 75, 80],
+"Age": [23, 25, 30, 22, 28, 26, 21, 27],
+"JoinDate": pd.to_datetime([
+"2020-01-10", "2020-02-15", "2021-03-20", "2021-04-10",
+"2020-05-30", "2020-06-25", "2021-07-15", "2021-08-01"
+])
+})
+df = df.groupby("Department")["Salary"].mean()
 print(df)
